c3f25b6b2d45d78fa9478560d795e954dec514a9
Jesus Cuesta <jesus@jesuscuesta.net>
2020-08-28 20:42:16 +0200

    feat(stable)

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,22 +1,17 @@
 <script>
-	import Nav from '../components/Nav.svelte';
+  import Nav from "../components/Nav.svelte";
 
-	export let segment;
+  export let segment;
 </script>
 
 <style>
-	main {
-		position: relative;
-		max-width: 56em;
-		background-color: white;
-		padding: 2em;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
+
 </style>
 
-<Nav {segment}/>
+<div id="page-wrapper">
+  <Nav {segment} />
 
-<main>
-	<slot></slot>
-</main>
+  <main>
+    <slot />
+  </main>
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,46 +1,235 @@
-<style>
-	h1, figure, p {
-		text-align: center;
-		margin: 0 auto;
-	}
-
-	h1 {
-		font-size: 2.8em;
-		text-transform: uppercase;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	}
-
-	figure {
-		margin: 0 0 1em 0;
-	}
-
-	img {
-		width: 100%;
-		max-width: 400px;
-		margin: 0 0 1em 0;
-	}
-
-	p {
-		margin: 1em auto;
-	}
-
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
-	}
-</style>
-
 <svelte:head>
-	<title>Sapper project template</title>
+  <title>Svelte Madrid</title>
 </svelte:head>
 
-<h1>Great success!</h1>
+<!-- Header https://html5up.net/landed -->
 
-<figure>
-	<img alt='Success Kid' src='successkid.jpg'>
-	<figcaption>Have fun with Sapper!</figcaption>
-</figure>
+<!-- Banner -->
+<section id="banner">
+  <div class="content">
+    <header>
+      <h2>Svelte Madrid</h2>
+      <p>
+        Aplicaciones & Arquitecturas
+        <br />
+        Compartimos Conocimiento
+      </p>
+    </header>
+    <span class="image">
+      <img src="./images/pic01.jpg" alt="" />
+    </span>
+  </div>
+  <a href="#one" class="goto-next scrolly">Next</a>
+</section>
 
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
+<!-- One -->
+<section id="one" class="spotlight style1 bottom">
+  <span class="image fit main">
+    <img src="images/pic02.jpg" alt="" />
+  </span>
+  <div class="content">
+    <div class="container">
+      <div class="row">
+        <div class="col-4 col-12-medium">
+          <header>
+            <h2>Comunidad Tecnológica</h2>
+            <p>Charlas, eventos, aplicaciones y arquitecturas</p>
+          </header>
+        </div>
+        <div class="col-4 col-12-medium">
+          <p>
+            Nuestra comunidad comparte los conocimientos de Svelte, dando charlas, creando aplicaciones, asistiendo en eventos y probando las diferentes posibilidades que puede aportar esta librería.
+			Pásate por nuestro Meetup y únete.
+          </p>
+        </div>
+        <div class="col-4 col-12-medium">
+          <p>
+            Todo el mundo con energía, valores y con ganas de compartir siempre es bienvenido. Tanto si estás empezando, como si has utilizado Svelte con diferentes arquitecturas, estás invitado a compartir
+			con el resto de la comunidad.
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <a href="#five" class="goto-next scrolly">Next</a>
+</section>
+
+<!-- Two -->
+<!-- <section id="two" class="spotlight style2 right">
+  <span class="image fit main">
+    <img src="images/pic03.jpg" alt="" />
+  </span>
+  <div class="content">
+    <header>
+      <h2>Interdum amet non magna accumsan</h2>
+      <p>Nunc commodo accumsan eget id nisi eu col volutpat magna</p>
+    </header>
+    <p>
+      Feugiat accumsan lorem eu ac lorem amet ac arcu phasellus tortor enim mi
+      mi nisi praesent adipiscing. Integer mi sed nascetur cep aliquet augue
+      varius tempus lobortis porttitor lorem et accumsan consequat adipiscing
+      lorem.
+    </p>
+    <ul class="actions">
+      <li>
+        <a href="#" class="button">Learn More</a>
+      </li>
+    </ul>
+  </div>
+  <a href="#three" class="goto-next scrolly">Next</a>
+</section> -->
+
+<!-- Three -->
+<!-- <section id="three" class="spotlight style3 left">
+  <span class="image fit main bottom">
+    <img src="images/pic04.jpg" alt="" />
+  </span>
+  <div class="content">
+    <header>
+      <h2>Interdum felis blandit praesent sed augue</h2>
+      <p>Accumsan integer ultricies aliquam vel massa sapien phasellus</p>
+    </header>
+    <p>
+      Feugiat accumsan lorem eu ac lorem amet ac arcu phasellus tortor enim mi
+      mi nisi praesent adipiscing. Integer mi sed nascetur cep aliquet augue
+      varius tempus lobortis porttitor lorem et accumsan consequat adipiscing
+      lorem.
+    </p>
+    <ul class="actions">
+      <li>
+        <a href="#" class="button">Learn More</a>
+      </li>
+    </ul>
+  </div>
+  <a href="#four" class="goto-next scrolly">Next</a>
+</section> -->
+
+<!-- Four -->
+<!-- <section id="four" class="wrapper style1 special fade-up">
+  <div class="container">
+    <header class="major">
+      <h2>Accumsan sed tempus adipiscing blandit</h2>
+      <p>
+        Iaculis ac volutpat vis non enim gravida nisi faucibus posuere arcu
+        consequat
+      </p>
+    </header>
+    <div class="box alt">
+      <div class="row gtr-uniform">
+        <section class="col-4 col-6-medium col-12-xsmall">
+          <span class="icon solid alt major fa-chart-area" />
+          <h3>Ipsum sed commodo</h3>
+          <p>
+            Feugiat accumsan lorem eu ac lorem amet accumsan donec. Blandit orci
+            porttitor.
+          </p>
+        </section>
+        <section class="col-4 col-6-medium col-12-xsmall">
+          <span class="icon solid alt major fa-comment" />
+          <h3>Eleifend lorem ornare</h3>
+          <p>
+            Feugiat accumsan lorem eu ac lorem amet accumsan donec. Blandit orci
+            porttitor.
+          </p>
+        </section>
+        <section class="col-4 col-6-medium col-12-xsmall">
+          <span class="icon solid alt major fa-flask" />
+          <h3>Cubilia cep lobortis</h3>
+          <p>
+            Feugiat accumsan lorem eu ac lorem amet accumsan donec. Blandit orci
+            porttitor.
+          </p>
+        </section>
+        <section class="col-4 col-6-medium col-12-xsmall">
+          <span class="icon solid alt major fa-paper-plane" />
+          <h3>Non semper interdum</h3>
+          <p>
+            Feugiat accumsan lorem eu ac lorem amet accumsan donec. Blandit orci
+            porttitor.
+          </p>
+        </section>
+        <section class="col-4 col-6-medium col-12-xsmall">
+          <span class="icon solid alt major fa-file" />
+          <h3>Odio laoreet accumsan</h3>
+          <p>
+            Feugiat accumsan lorem eu ac lorem amet accumsan donec. Blandit orci
+            porttitor.
+          </p>
+        </section>
+        <section class="col-4 col-6-medium col-12-xsmall">
+          <span class="icon solid alt major fa-lock" />
+          <h3>Massa arcu accumsan</h3>
+          <p>
+            Feugiat accumsan lorem eu ac lorem amet accumsan donec. Blandit orci
+            porttitor.
+          </p>
+        </section>
+      </div>
+    </div>
+    <footer class="major">
+      <ul class="actions special">
+        <li>
+          <a href="#" class="button">Magna sed feugiat</a>
+        </li>
+      </ul>
+    </footer>
+  </div>
+</section> -->
+
+<!-- Five -->
+<section id="five" class="wrapper style2 special fade">
+  <div class="container">
+    <header>
+      <h2>Newsletter de Javascript</h2>
+      <p>Te enviaremos novedades de javascript, eventos... y Svelte JS</p>
+    </header>
+    <form method="post" action="http://eepurl.com/gD9huD" class="cta" target="_blank">
+      <div class="row gtr-uniform gtr-50">
+        <div class="col-4 col-12-xsmall" style="margin: 0 auto;">
+          <input type="submit" value="Subscríbete" class="fit primary" />
+        </div>
+      </div>
+    </form>
+  </div>
+</section>
+
+<!-- Footer -->
+<footer id="footer">
+  <ul class="icons">
+    <li>
+      <a href="https://twitter.com/sveltemadrid" class="icon brands alt fa-twitter">
+        <span class="label">Twitter</span>
+      </a>
+    </li>
+	<li>
+      <a href="https://www.meetup.com/es-ES/Svelte-Madrid/" class="icon brands alt fa-meetup">
+        <span class="label">Facebook</span>
+      </a>
+    </li>
+    <!-- <li>
+      <a href="#" class="icon brands alt fa-facebook-f">
+        <span class="label">Facebook</span>
+      </a>
+    </li>
+    <li>
+      <a href="#" class="icon brands alt fa-linkedin-in">
+        <span class="label">LinkedIn</span>
+      </a>
+    </li>
+    <li>
+      <a href="#" class="icon brands alt fa-instagram">
+        <span class="label">Instagram</span>
+      </a>
+    </li>
+    <li>
+      <a href="#" class="icon brands alt fa-github">
+        <span class="label">GitHub</span>
+      </a>
+    </li>
+    <li>
+      <a href="#" class="icon solid alt fa-envelope">
+        <span class="label">Email</span>
+      </a>
+    </li> -->
+  </ul>
+</footer>
